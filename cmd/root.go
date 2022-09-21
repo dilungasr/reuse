87d5a8e26ac1a3d709a9bb6c30e66078ca73d061
Dilunga SR <dilungasalumtz@gmail.com>
2022-09-22 02:03:59 +0300

    Removed start command and improved instructions

--- cmd/root.go
+++ cmd/root.go
@@ -4,14 +4,20 @@ Copyright © 2022 Dilunga SR <dilungasr@gmail.com>
 package cmd
 
 import (
+	"fmt"
 	"os"
+	"path/filepath"
+	"time"
 
+	"github.com/dilungasr/reuse/errs"
+	"github.com/dilungasr/reuse/handlers"
+	"github.com/dilungasr/reuse/logger"
 	"github.com/spf13/cobra"
 )
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "goreuse",
+	Use:   "reuse",
 	Short: "Replace module names and everything you need to reuse your project",
 	Long: `Go Reuse provides a simple way to reuse your project to create a new project with 
 	some little changes.
@@ -26,6 +32,62 @@ name.
 Don't start from scratch, don't get stressed...
 Just reuse it :)
 `,
+
+	Run: func(cmd *cobra.Command, args []string) {
+		// starting time
+		start := time.Now()
+
+		// projectPath defaults to the current directory
+		projectPath := "."
+
+		// if project path given then use it
+		if len(args) >= 1 && args[0] != "" {
+			projectPath = filepath.FromSlash(args[0])
+		}
+
+		// look for the flags provided
+		use, err := cmd.Flags().GetString("use")
+		errs.Check(err)
+
+		IsInteractive, err := cmd.Flags().GetBool("interact")
+		errs.Check(err)
+
+		IsConfigProvided := use != ""
+
+		// interactively take configurations from the user
+		// if it's in interative mode
+		if IsInteractive {
+			handlers.ReadFromInput()
+		} else if IsConfigProvided {
+			// read the configuration file if there is a config file to use
+			handlers.ReadConfig(use, projectPath)
+		} else {
+			// search configuration file with default config name 'reuse.supportedExt'
+			ok := handlers.FindConfigAndRead(projectPath)
+
+			if !ok {
+				fmt.Println("Attempting to open in interactive mode...")
+				handlers.ReadFromInput()
+			}
+		}
+
+		// // start iterating for deleting and replacing
+
+		fmt.Println("")
+		fmt.Println("")
+		fmt.Println("Starts modifying contents....")
+		start2 := time.Now()
+		fmt.Println("")
+
+		handlers.Iterate(projectPath)
+
+		// time eplapsed
+		logger.Elapsed(start2, "Finished modifying contents in")
+
+		// // finish by running the commands
+		handlers.Run(projectPath)
+		logger.Elapsed(start, ";) You are almost there in just")
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -46,5 +108,9 @@ func init() {
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.PersistentFlags().StringP("use", "u", "", `Provides a path to the configuration file if any. 
+	You do not need this in interactive mode`,
+	)
+	rootCmd.PersistentFlags().BoolP("interact", "i", false, `Start reuse process in interactive mode`)
 }
