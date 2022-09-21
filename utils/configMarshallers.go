b31231dfdc6c019cb82a5a2dc7d61007e02f1e1c
Dilunga SR <dilungasalumtz@gmail.com>
2022-09-21 20:24:48 +0300

    Add config marshallers for reading supported config files

---
+++ utils/configMarshallers.go
@@ -0,0 +1,53 @@
+package utils
+
+import (
+	"encoding/json"
+	"log"
+	"os"
+
+	"github.com/dilungasr/reuse/logger"
+	"github.com/dilungasr/reuse/models"
+	"github.com/pelletier/go-toml/v2"
+	"gopkg.in/yaml.v3"
+)
+
+// readConfigContent reads the content of the named file
+// and returns them as []byte
+func readConfigContent(filePath string) (content []byte) {
+	content, err := os.ReadFile(filePath)
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	return content
+}
+
+// ReadJSON reads the project changes' file  as JSON
+func ReadJSON(filePath string) {
+	content := readConfigContent(filePath)
+
+	err := json.Unmarshal(content, &models.ProjectChanges)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// ReadYAML reads the project changes' file as YAML
+func ReadYAML(filePath string) {
+	content := readConfigContent(filePath)
+
+	err := yaml.Unmarshal(content, &models.ProjectChanges)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// ReadTOML reads the project changes' file as TOML
+func ReadTOML(filePath string) {
+	content := readConfigContent(filePath)
+
+	err := toml.Unmarshal(content, &models.ProjectChanges)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
